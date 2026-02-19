d094b33536c757a964279c52b7aa2376a7d3b902
Jean Lucio <jeanlucio@gmail.com>
2026-02-19 00:06:18 -0300

    Moodle Coding Style

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_playerhud';
-$plugin->version   = 2026021800;
+$plugin->version   = 2026021801;
 $plugin->requires  = 2024100700;        // Requires: Moodle 4.5+.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'v1.0';
