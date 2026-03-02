8cbae76963c156ed545673e1db4da57d600c1c11
Jean Lúcio <jeanlucio@gmail.com>
2026-03-02 03:21:00 +0000

    style: fix SCSS linting errors

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_playerhud';
-$plugin->version   = 2026030100;
+$plugin->version   = 2026030200;
 $plugin->requires  = 2024100700;        // Requires: Moodle 4.5+.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.0.3';
+$plugin->release   = 'v1.0.4';
 $plugin->dependencies = ['block_playerhud' => 2026021800];
