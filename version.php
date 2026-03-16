64802391d454f2cac955c95dc9d231578bca93ca
Jean Lúcio <jeanlucio@gmail.com>
2026-03-16 20:26:40 +0000

    Resolve dark mode contrast accessibility bug

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_playerhud';
-$plugin->version   = 2026030201;
+$plugin->version   = 2026031600;
 $plugin->requires  = 2024100700;        // Requires: Moodle 4.5+.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.0.5';
+$plugin->release   = 'v1.0.6';
 $plugin->dependencies = ['block_playerhud' => 2026021800];
