133ab1475c3ade2bf817743bbdbe56163a0c7580
Jean Lúcio <jeanlucio@gmail.com>
2026-03-31 20:40:54 +0000

    ci: update test matrix for Moodle 5.2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_playerhud';
-$plugin->version   = 2026033001;
+$plugin->version   = 2026033100;
 $plugin->requires  = 2024100700;        // Requires: Moodle 4.5+.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.1.1';
+$plugin->release   = 'v1.1.2';
 $plugin->dependencies = ['block_playerhud' => 2026021800];
